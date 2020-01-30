0662160a53274627eb77eb72912b79de9a1a2baa
Neo <c.min.tseng@gmail.com>
2020-01-30 18:56:33 +0800

    temp save

---
+++ RTSP-IPCam-Viewer/src/main/java/com/github/warren_bank/rtsp_ipcam_viewer/mock/data/C.java
@@ -0,0 +1,46 @@
+package com.github.warren_bank.rtsp_ipcam_viewer.mock.data;
+
+public class C {
+    public static final String SPHERICAL_STEREO_MODE_EXTRA = "spherical_stereo_mode";
+    public static final String SPHERICAL_STEREO_MODE_MONO = "mono";
+    public static final String SPHERICAL_STEREO_MODE_TOP_BOTTOM = "top_bottom";
+    public static final String SPHERICAL_STEREO_MODE_LEFT_RIGHT = "left_right";
+
+    // Actions.
+
+    public static final String ACTION_VIEW = "com.google.android.exoplayer.demo.action.VIEW";
+    public static final String ACTION_VIEW_LIST = "com.google.android.exoplayer.demo.action.VIEW_LIST";
+
+    // Player configuration extras.
+
+    public static final String ABR_ALGORITHM_EXTRA = "abr_algorithm";
+    public static final String ABR_ALGORITHM_DEFAULT = "default";
+    public static final String ABR_ALGORITHM_RANDOM = "random";
+
+    // Media item configuration extras.
+
+    public static final String URI_EXTRA = "uri";
+    public static final String EXTENSION_EXTRA = "extension";
+    public static final String IS_LIVE_EXTRA = "is_live";
+
+    public static final String DRM_SCHEME_EXTRA = "drm_scheme";
+    public static final String DRM_LICENSE_URL_EXTRA = "drm_license_url";
+    public static final String DRM_KEY_REQUEST_PROPERTIES_EXTRA = "drm_key_request_properties";
+    public static final String DRM_SESSION_FOR_CLEAR_TYPES_EXTRA = "drm_session_for_clear_types";
+    public static final String DRM_MULTI_SESSION_EXTRA = "drm_multi_session";
+    public static final String PREFER_EXTENSION_DECODERS_EXTRA = "prefer_extension_decoders";
+    public static final String TUNNELING_EXTRA = "tunneling";
+    public static final String AD_TAG_URI_EXTRA = "ad_tag_uri";
+    public static final String SUBTITLE_URI_EXTRA = "subtitle_uri";
+    public static final String SUBTITLE_MIME_TYPE_EXTRA = "subtitle_mime_type";
+    public static final String SUBTITLE_LANGUAGE_EXTRA = "subtitle_language";
+    // For backwards compatibility only.
+    public static final String DRM_SCHEME_UUID_EXTRA = "drm_scheme_uuid";
+
+    // Saved instance state keys.
+
+    private static final String KEY_TRACK_SELECTOR_PARAMETERS = "track_selector_parameters";
+    private static final String KEY_WINDOW = "window";
+    private static final String KEY_POSITION = "position";
+    private static final String KEY_AUTO_PLAY = "auto_play";
+}
